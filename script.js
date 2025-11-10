55d90ab72db92292abf0749fad9600aff1622e72
Asraful Islam <asrafulislam.12967@gmail.com>
2025-11-10 18:29:33 +0600

    This is the second commit, half way to the javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get all accordion items
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+  // Loop through each accordion item 
+  accordionItems.forEach(item =>
+     // Get the header button for each item
+     const header = item.querySelector
